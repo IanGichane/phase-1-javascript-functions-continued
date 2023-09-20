dbbe17937a1dda75d8c0f949bdc22ef433378fdb
Ian <gichaneian@gmail.com>
2023-09-20 14:24:24 +0300

    Completed functions lab,hoisting,scope..

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+ return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+function mondayWork(task='go to the office'){
+    return `This Monday, I will ${task}.`
+}
+mondayWork();
+
+function wrapAdjective(string="*"){
+    return function innerFunc(param='special'){
+        return `You are ${string}${param}${string}!`
+    }
+}
+const x = wrapAdjective('||')
